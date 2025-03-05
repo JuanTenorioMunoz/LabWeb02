95ee173d8295222163061ab49c6b1bcae935b75b
Camilo Game <tenorio2003@gmail.com>
2025-03-05 10:51:33 -0500

    add_localStorage

--- src/components/MyForm/MyForm.jsx
+++ src/components/MyForm/MyForm.jsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 
 const MyForm = () =>{
+
+    const [newTask, setTask] = useState("")
+    const [taskNumber, setTaskNumber] = useState(0)
     
     const handleSubmit = (event) => {
 		event.preventDefault();
+        addTask(newTask);
         setTask("");
 	};
 
-    const addTask = (event) => {
+    const writeTask = (event) => {
         setTask(event.target.value);
         console.log(newTask);
     }
 
-    const [newTask, setTask] = useState("")
+    const addTask = (receivedTask) => {
+        localStorage.setItem(taskNumber, receivedTask)
+        console.log("task written " + receivedTask)
+        setTaskNumber(number => number + 1)
+        console.log("task number " + taskNumber)
+    }
+
     
     return(
 
         <form className="form-container" onSubmit={handleSubmit}>
-            <input type="text" onChange={addTask} value={newTask}></input>
+            <input type="text" onChange={writeTask} value={newTask}></input>
             <button type="submit">sumbbbbit</button>
         </form>
     )
