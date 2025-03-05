c0ba1244b24adcbf3e5500f2f930beab89d83df6
Camilo Game <tenorio2003@gmail.com>
2025-03-05 12:25:45 -0500

    fix_getKeyandValue from local

--- src/components/TasksContainer/TasksContainer.jsx
+++ src/components/TasksContainer/TasksContainer.jsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const TasksContainer = ({status}) => {
 
+    const [taskCount, setTaskCount] = useState(0)
+
+
     useEffect(()=> {
-        console.log(localStorage)
+        Object.keys(localStorage).forEach(key => {
+            console.log(key + localStorage.getItem(key))
+        });
+
     },[taskCount])
-    
-    
 
     return(
         <div className={status + "-TasksContainer"}>
